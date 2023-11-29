28062f68373b4a44eecb03f73e416e1b50544c16
EVERYTHING' FUCK <104085499+XYTEEE@users.noreply.github.com>
2023-11-29 12:38:15 +0600

    Update Picchi.py

--- Picchi.py
+++ Picchi.py
@@ -1,14 +1,7 @@
-import os, sys, platform
-try:os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
-except:pass
-try:os.system('git pull')
-except:pass
-try:os.system('am start https://t.me/xyteeetools')
-except:pass
-bit = platform.architecture()[0]
-if bit == '64bit':
-    import xcmain
-    xcmain.Rand()
-elif bit == '32bit':
-    os.system("exit")
- 
+import os,time
+os.system('clear')
+os.system('xdg-open https://t.me/xyteeetools')
+try:
+    __import__("xcmain").main()
+except Exception as e:
+    exit(str(e))

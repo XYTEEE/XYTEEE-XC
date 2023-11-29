45e80793b3944e3703dc1fbe7320023ed6959795
EVERYTHING' FUCK <104085499+XYTEEE@users.noreply.github.com>
2023-11-29 12:30:08 +0600

    Update Picchi.py

--- Picchi.py
+++ Picchi.py
@@ -1,11 +1,8 @@
-import os,time
-os.system('clear')
-os.system('xdg-open https://t.me/xyteeetools')
-from platform import uname
-arch=uname().machine.lower()
-if 'aarch' in arch:
-    print(' \n\033[1;37m Congratulations! Your Device Support This Tools');time.sleep(2)
-    import xcmain.Rand()
-else:
-    print(' \033[1;31m Sorry system or device not support this tools');exit()
+ 
+import os, sys
+try:
+    __import__("xcmain").main()
+except Exception as e:
+    exit(str(e))
+  
  

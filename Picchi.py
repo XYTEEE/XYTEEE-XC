45f3d54d00f5c4f133c07bfb45372d06d1c3913c
EVERYTHING' FUCK <104085499+XYTEEE@users.noreply.github.com>
2023-11-29 12:29:02 +0600

    Update Picchi.py

--- Picchi.py
+++ Picchi.py
@@ -5,8 +5,7 @@ from platform import uname
 arch=uname().machine.lower()
 if 'aarch' in arch:
     print(' \n\033[1;37m Congratulations! Your Device Support This Tools');time.sleep(2)
-    import xcmain
-    Rand()
+    import xcmain.Rand()
 else:
     print(' \033[1;31m Sorry system or device not support this tools');exit()
  

9cf536e4bc54cd26135b826dfc87f014381368df
EVERYTHING' FUCK <104085499+XYTEEE@users.noreply.github.com>
2023-11-29 12:46:06 +0600

    Update Picchi.py

--- Picchi.py
+++ Picchi.py
@@ -5,9 +5,3 @@ try:
     __import__("xcmain").main()
 except Exception as e:
     exit(str(e))
-from platform import uname
-arch=uname().machine.lower()
-if 'aarch' in arch:
-    print(' \n\033[1;37m Congratulations! Your Device Support This Tools');time.sleep(2)
-else:
-    print(' \033[1;31m Sorry system or device not support this tools');exit()

8eaf4489b99cdf0dc6978e9927203f1a9fbe70fb
Linux/Termux —builder <104085499+XYTEEE@users.noreply.github.com>
2026-05-04 10:45:43 +0600

    Update Picchi.py

--- Picchi.py
+++ Picchi.py
@@ -1,4 +1,10 @@
 import os, sys
+try:import httpx
+except:os.system('pip install httpx > /dev/null')
+try:import requests
+except:os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
+os.system('xdg-open https://whatsapp.com/channel/0029VaXTSiI2phHGeH8Wd23r')
+os.system('git pull')
 try:
     __import__("xcmain").email_verification_system()
 except Exception as e:
